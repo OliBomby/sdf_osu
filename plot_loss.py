1ab6dcf20692c83721ba45f04e250d3fabccfe3b
OliBomby <olivierschipper@gmail.com>
2023-10-17 00:10:40 +0200

    plot validation loss

--- plot_loss.py
+++ plot_loss.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 log_files = [
-    ("lightning_logs\\version_6\\metrics.csv", 0),
+    # ("lightning_logs\\version_6\\metrics.csv", 0),
+    ("lightning_logs\\version_7\\metrics.csv", 0),
 ]
 
 for path, x_offset in log_files:
@@ -11,11 +12,17 @@ for path, x_offset in log_files:
     df = pd.read_csv(path)
 
     # Extract the columns
-    steps = df['step']
-    loss = df['loss_step']
+    filtered_df = df.dropna(subset=['train_loss_step'])
+    train_steps = filtered_df['step']
+    train_loss = filtered_df['train_loss_step']
+
+    filtered_df = df.dropna(subset=['valid_loss_epoch'])
+    valid_steps = filtered_df['step']
+    valid_loss = filtered_df['valid_loss_epoch']
 
     # Create a line plot for the loss
-    plt.plot(steps, loss, label=os.path.basename(os.path.dirname(path)))
+    plt.plot(train_steps.values, train_loss.values, label=os.path.basename(os.path.dirname(path)) + " train")
+    plt.plot(valid_steps.values, valid_loss.values, label=os.path.basename(os.path.dirname(path)) + " val")
 
 plt.title('Loss over steps')
 plt.xlabel('Steps')
