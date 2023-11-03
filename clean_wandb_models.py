df714c91ee7598acdcdb4f3bf8dd59a05ab520b2
OliBomby <olivierschipper@gmail.com>
2023-11-03 12:21:45 +0100

    add script to clean saved wandb models

---
+++ clean_wandb_models.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# https://community.wandb.ai/t/using-the-python-api-to-delete-models-with-no-tag-minimal/1498?u=turian
+# "Rather than using api.artifact_versions, it uses the versions
+# method on artifact_collection."
+
+import wandb
+from tqdm.auto import tqdm
+
+# dry_run = True
+dry_run = False
+api = wandb.Api()
+project = api.project("sdf-osu")
+
+for artifact_type in project.artifacts_types():
+    if artifact_type.type != "model":
+        continue
+    collection_versions = []
+    for artifact_collection in tqdm(artifact_type.collections()):
+        for version in artifact_collection.versions():
+            if version.state != "DELETED":
+                collection_versions.append((artifact_collection, version))
+
+for (artifact_collection, version) in tqdm(collection_versions):
+    if len(version.aliases) > 0:
+        # print out the name of the one we are keeping
+        print(f"KEEPING {version.name} {version.aliases}")
+    else:
+        if not dry_run:
+            version.delete()
+        else:
+            print("")
+            print(f"should delete {version.name}")
